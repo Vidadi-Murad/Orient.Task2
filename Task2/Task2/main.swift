98494d51d122becf477b3c9251cde1505ecb3c07
VidadiMurad <vidad0murad@gmail.com>
2026-05-07 09:11:25 +0400

    COMMIT

---
+++ Task2/Task2/main.swift
@@ -0,0 +1,54 @@
+//
+//  main.swift
+//  Task2
+//
+//  Created by Vidadi Muradzada on 06.05.26.
+//
+
+import Foundation
+
+/*
+var num : Int = 1
+
+for i in num...20{
+    if i % 2 == 0 {
+        print(i)
+    }
+    
+}
+
+*/
+
+
+/*
+var city: String? = "Bakı"
+var population: Int? = nil
+
+if let city = city{
+    print("Şəhər: \(city)")
+}
+if let population = population{
+    print(population)
+}else{
+    print("text")
+}
+*/
+
+
+/*func math(eded: Int?, bolen: Int?){
+    guard let eded = eded,
+          let bolen = bolen else {
+        return
+    }
+    
+    guard bolen != 0 else{
+        print("xeberdarliq")
+        return
+    }
+    
+    print(eded/bolen)
+}
+
+
+math(eded: 89, bolen: 7)
+*/
